c63dbb89ff40dd94553d99d57e8b94042c559dfd
FellipeMagalhaes <fellipe.augusto01@gmail.com>
2023-05-05 19:46:52 -0300

    Leitura de mensagens

--- src/main/java/Fellp/LeMensagem.java
+++ src/main/java/Fellp/LeMensagem.java
@@ -11,24 +11,16 @@ public class LeMensagem {
     public void LerMensagem(Mensagem msg, WebDriver webDriver){
 
         var elemento = findUltimaMensagem(webDriver);
+        msg.setConteudoRecebido(elemento.getText());
 
-        while (elemento == null){
 
-            msg.setConteudoRecebido(elemento.getText());
-            if (elemento != null){
-
-                msg.setConteudoRecebido(elemento.getText());
-                break;
-
-            }
-        }
 
     }
 
 
     public WebElement findUltimaMensagem(WebDriver webDriver){
 
-        var xPathCaixaTexto = "//*[@id=\"main\"]/div[2]/div/div[2]/div[2]/div[9]/div/div/div/div[1]/div[1]/div[1]/div/span[1]/span";
+        var xPathCaixaTexto = "//*[@id=\"pane-side\"]/div[1]/div/div/div[1]/div/div/div/div[2]/div[2]/div[1]/span/span[3]";
         return webDriver.findElement(By.xpath(xPathCaixaTexto));
 
     }
--- src/main/java/Fellp/StartApplication.java
+++ src/main/java/Fellp/StartApplication.java
@@ -1,22 +1,60 @@
 package Fellp;
 
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class StartApplication {
 
     public static void main(String[] args) {
 
+    }
 
-        Mensagem msg = new Mensagem();
-        msg.setConteudo("Salve salve rapaziada");
-        msg.setContatos("Edielson [SENAI]");
-        EnviaMensagem Emsg = new EnviaMensagem();
-        LeMensagem Lmsg = new LeMensagem();
+    public static void MSGTeste(){
+
         IntegraçãoChrome Ic = new IntegraçãoChrome();
         Ic.webDriver();
-        var elemento = Emsg.findContato(msg.getContatos(), Ic.getWebDriver());
-        elemento.click();
-        Lmsg.LerMensagem(msg, Ic.getWebDriver());
-        System.out.println(msg.getConteudoRecebido());
+        Mensagem msg = new Mensagem();
+        msg.setContatos("Luiz [TSE]");
+        msg.setConteudo("Palmeiras não tem mundial");
+        EnviaMensagem Env = new EnviaMensagem();
+        Env.EnviarMensagem(msg, Ic.getWebDriver());
 
     }
+
+    public static void IngredienteTeste(){
+
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList <Ingrediente> Menu = new ArrayList<>();
+
+
+       String option = "";
+        while (option.equals("n") != true){
+
+            Ingrediente tIngrediente = new Ingrediente();
+            tIngrediente.criaIngrediente();
+            Menu.add(tIngrediente);
+            System.out.print("Add new Ingrediente? ");
+            option = scanner.nextLine();
+
+        }
+
+        for(int n = 0; n < Menu.size(); n++ ){
+
+            System.out.println(Menu.get(n).getNome());
+            System.out.println("\n");
+            System.out.println(Menu.get(n).getPesoUNI());
+            System.out.println("\n");
+            System.out.println(Menu.get(n).getPesoREC());
+            System.out.println("\n");
+            System.out.println(Menu.get(n).getPreco());
+            System.out.println("\n");
+
+        }
+
+
+    }
+
 }
